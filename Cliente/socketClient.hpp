9f217138daacc631457ddc6e209eeef6876a6d57
Lucas Yuji Matubara <yuji.matubara@outlook.com>
2021-06-07 14:47:02 -0300

    adding socketClient.hpp

---
+++ Cliente/socketClient.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <unordered_map>
+
+class Client{
+    private:
+
+
+    public:
+
+
+};
